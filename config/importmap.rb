16f3fd5feaaa2e505beaa78b2e48e8c46ff2ea5b
Alex Candido <alex.candido.tec@gmail.com>
2025-02-03 11:53:36 -0300

    instalado leaflet, leaflet-provider, leaflet.markercluster

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "preline", to: "https://cdn.jsdelivr.net/npm/preline@2.7.0/index.js" # 2.7.0
+pin "leaflet", to: "https://ga.jspm.io/npm:leaflet@1.9.4/dist/leaflet-src.js" # @1.9.4
+pin "leaflet-providers", to: "https://ga.jspm.io/npm:leaflet-providers@2.0.0/leaflet-providers.js" # @2.0.0
+pin "leaflet.markercluster", to: "https://ga.jspm.io/npm:leaflet.markercluster@1.5.3/dist/leaflet.markercluster-src.js" # @1.5.3
